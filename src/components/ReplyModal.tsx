7b9af06c9a7702b33cca62fb09cd5300a07906dc
Abdul Rehman <ar618211@gmail.com>
2023-06-10 18:02:43 +0500

    broke form comp

--- src/components/ReplyModal.tsx
+++ src/components/ReplyModal.tsx
@@ -3,18 +3,15 @@ import { api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoaderSpinner } from "./loading";
 import { useUser } from "@clerk/nextjs";
 import { useReplyModal } from "~/store";
-import { useState } from "react";
 import CloseSVG from "~/svgs/CloseSVG";
 import { toast } from "react-hot-toast";
+import ReplyTweetForm from "./ReplyTweetForm";
 
 dayjs.extend(relativeTime);
 
 function ReplyModal() {
-  const [ input, setInput ] = useState('');
-
   const { active, post, closeModal } = useReplyModal();
 
   const { user } = useUser();
@@ -25,14 +22,12 @@ function ReplyModal() {
 
   if (!post || !active || !user) return null;  
 
-  function replyTweet(e: React.SyntheticEvent) {
-    e.stopPropagation();
-    e.preventDefault();
+  function replyTweet(input: string) {
     if (!user) {
       toast.error('Please log in first!')
       return;
     }
-    if (!input || !post) return;
+    if (!post) return;
 
     mutate({
       content: input,
@@ -87,38 +82,7 @@ function ReplyModal() {
                 <p className="mb-2">{post.post.content}</p>
               </div>
             </div>
-            <form onSubmit={replyTweet} className="flex flex-row px-4 gap-x-2 py-2 border-b border-slate-400">
-              <div className="bg-slate-400 rounded-full w-12 h-12 overflow-hidden relative">
-                <Image
-                  src={user.profileImageUrl}
-                  alt={`@${user.username ?? ''}'s profile picture`}
-                  fill
-                />
-              </div>
-              <div className="flex-grow flex flex-col">
-                <input
-                  type="text"
-                  name="tweet"
-                  id="tweet"
-                  value={input}
-                  onChange={(e) => {setInput(e.target.value)}}
-                  placeholder="Tweet your reply"
-                  className="text-lg bg-transparent focus-visible:outline-none p-2 block"
-                  required
-                  disabled={isPosting}
-                />
-                <div className="flex flex-row justify-end">
-                  {isPosting &&
-                    <div className="h-fit self-center"><LoaderSpinner size={24} /></div>
-                  }
-                  <button
-                    className="px-4 py-2 text-md bg-sky-500 rounded-3xl"
-                    type="submit"
-                    disabled={isPosting}
-                  >Reply</button>
-                </div>
-              </div>
-            </form>
+            <ReplyTweetForm onSubmit={replyTweet} isPosting={isPosting} />
           </div>
         </div>
       </div>
---
+++ src/components/ReplyTweetForm.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import React, { useState } from "react"
+import { LoaderSpinner } from "./loading"
+import { useUser } from "@clerk/nextjs"
+
+const ReplyTweetForm = ({ isPosting, onSubmit }: {
+  isPosting: boolean
+  onSubmit: (input: string) => void
+}) => {
+  const { user } = useUser();
+
+  const [ input, setInput ] = useState<string>('')
+
+  if (!user) return null;
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (!input) return;
+
+      onSubmit(input)
+    }} className="flex flex-row px-4 gap-x-2 py-2 border-b border-slate-400">
+      <div className="bg-slate-400 rounded-full w-12 h-12 overflow-hidden relative">
+        <Image
+          src={user.profileImageUrl}
+          alt={`@${user.username ?? ''}'s profile picture`}
+          fill
+        />
+      </div>
+      <div className="flex-grow flex flex-col">
+        <input
+          type="text"
+          name="tweet"
+          id="tweet"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value)
+          }}
+          placeholder="Tweet your reply"
+          className="text-lg bg-transparent focus-visible:outline-none p-2 block"
+          required
+          disabled={isPosting}
+        />
+        <div className="flex flex-row justify-end">
+          {isPosting &&
+            <div className="h-fit self-center"><LoaderSpinner size={24} /></div>
+          }
+          <button
+            className="px-4 py-2 text-md bg-sky-500 rounded-3xl"
+            type="submit"
+            disabled={isPosting}
+          >Reply</button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default ReplyTweetForm;
