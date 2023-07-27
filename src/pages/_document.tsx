3d9b705b24505e7518a86bec42d828e84dfe43d6
Abdul Rehman <ar618211@gmail.com>
2023-07-27 18:12:15 +0500

    added document and engyj widget

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script defer src="https://recruitchat.engyj.com/chatbotsystem/0.1/script.js?user_id=41"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
