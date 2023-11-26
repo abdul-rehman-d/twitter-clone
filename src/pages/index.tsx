28e592371dd946611ea770c4b1d26ddd9d72595a
Abdul Rehman <ar618211@gmail.com>
2023-11-26 19:18:16 +0500

    fix: ask for username at sign in if auth via google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,6 +88,95 @@ const CreatePost = () => {
   );
 };
 
+const CheckUserNameAndAssignOneModal = () => {
+  const { user } = useUser();
+
+  const {
+    mutate,
+    isLoading: isSubmitting,
+    error,
+    isError,
+    isSuccess,
+  } = api.profile.assignUsername.useMutation();
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (isSubmitting) return;
+    const username = (e.target as HTMLFormElement).username.value;
+    if (username) {
+      mutate(
+        {
+          username,
+        },
+        {
+          onSuccess: () => {
+            toast.success("Username updated successfully!");
+          },
+          onError: (err) => {},
+        }
+      );
+    }
+  };
+
+  if (!user) return null;
+
+  if (user.username) {
+    return null;
+  }
+
+  if (isSuccess) {
+    return null;
+  }
+
+  return (
+    <div
+      tabIndex={-1}
+      aria-hidden="true"
+      className="fixed top-0 left-0 right-0 z-50 flex h-full max-h-full min-h-screen w-full items-center justify-center overflow-y-auto overflow-x-hidden bg-[#495a6966] p-4 md:inset-0"
+    >
+      <div className="relative max-h-full w-full max-w-xl">
+        <div className="relative rounded-2xl bg-black">
+          <div>
+            <div className=" p-4">
+              <div className="">
+                <div className="my-4 text-center text-2xl text-slate-100">
+                  <h2>Pick a username</h2>
+                  <p className="text-base text-slate-300">
+                    We are sorry but you cannot proceed before you pick a
+                    username.
+                  </p>
+                </div>
+                <form onSubmit={onSubmit} className="mx-4">
+                  <div className="mb-4 flex flex-row flex-wrap gap-4">
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      placeholder="Username"
+                      className="text-md block flex-grow rounded-md border border-slate-400 bg-transparent p-2 outline-none focus-visible:outline-none"
+                      required
+                    />
+                    <button
+                      className="text-md rounded-3xl bg-sky-500 px-4 py-2"
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      {isSubmitting ? <LoaderSpinner /> : "Submit"}
+                    </button>
+                  </div>
+                  {isError && (
+                    <div className="text-red-500">{error.message}</div>
+                  )}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.post.getAll.useQuery();
 
@@ -122,7 +211,10 @@ const Home: NextPage = () => {
     <>
       <PageLayout headerText="Home">
         {userSignedIn ? (
-          <CreatePost />
+          <>
+            <CreatePost />
+            <CheckUserNameAndAssignOneModal />
+          </>
         ) : (
           <div className="flex w-full flex-row justify-center border-b border-slate-400 py-2">
             <div className="text-md rounded-3xl bg-sky-500 px-4 py-2 font-semibold">
--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -2,7 +2,11 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import filterUserForClient from "~/server/helpers/filterUserForClient";
 
 export const profileRouter = createTRPCRouter({
@@ -23,5 +27,38 @@ export const profileRouter = createTRPCRouter({
         });
       }
       return filterUserForClient(user);
+    }),
+  assignUsername: privateProcedure
+    .input(
+      z.object({
+        username: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await clerkClient.users.getUser(ctx.currentUser);
+      if (!user) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "User not found",
+        });
+      }
+
+      // check if taken
+      const [existingUser] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
+
+      if (existingUser) {
+        console.log("existingUser", existingUser);
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Username taken",
+        });
+      }
+
+      // update username
+      await clerkClient.users.updateUser(ctx.currentUser, {
+        username: input.username,
+      });
     }),
 });
