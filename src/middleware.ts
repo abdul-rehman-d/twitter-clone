5253cfedd2a1929eb2ed19d98ac115015520c53c
Abdul Rehman <ar618211@gmail.com>
2024-03-30 20:57:32 +0500

    forcing deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+
 export default withClerkMiddleware(() => {
   return NextResponse.next();
 });
